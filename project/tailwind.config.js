3ea1a673156a09c78af67995e0fda145721354f3
alaotach <aryanmi2001@gmail.com>
2025-06-23 17:16:51 +0530

    export chat

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        '60': '60',
+        '70': '70',
+        '80': '80',
+        '90': '90',
+        '100': '100',
+        '999': '999',
+        '9999': '9999',
+      }
+    },
   },
   plugins: [],
 };
